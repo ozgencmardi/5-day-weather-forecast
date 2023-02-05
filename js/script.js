c3819d08759b98e022c299b6d7e2e98908566570
ozgencmardi <ozgencmardi@gmail.com>
2023-02-05 22:39:54 +0000

    Index and Sctip.js files updated

--- js/script.js
+++ js/script.js
@@ -11,4 +11,26 @@ $.ajax({
     method: "GET"
     }).then(function(response) {
         console.log(response);
-    });
+        updateCurrentWeather(response);
+        //updateForecast(response);
+        }).catch(function(error) {
+        console.error(error);
+        });
+      
+      function updateCurrentWeather(data) {
+        const city = data.city.name;
+        //console.log(city);
+        const date = new Date();
+        //console.log(date);
+        const icon = data.list[0].weather[0].icon;
+        //console.log(icon);
+        const temperature = data.list[0].main.temp;
+        //console.log(temperature);
+        const humidity = data.list[0].main.humidity;
+        //console.log(humidity)
+        const windSpeed = data.list[0].wind.speed;
+        //console.log(windSpeed)
+      
+        }
+
+      
